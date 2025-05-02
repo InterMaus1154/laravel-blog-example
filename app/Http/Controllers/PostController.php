b7ac38780c8facb1b5281b9ec48241d765c7e2de
intermaus1154 <amarco1154@gmail.com>
2025-05-02 10:53:52 +0100

    post creation form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\PostCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Log;
@@ -39,6 +40,8 @@ class PostController extends Controller
     // show post creating form
     public function create()
     {
-        return view('post.create');
+        // fetch categories
+        $categories = PostCategory::select('post_category_id', 'post_category_name')->get();
+        return view('post.create', compact('categories'));
     }
 }
