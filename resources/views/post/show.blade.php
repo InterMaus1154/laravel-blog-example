44fa436a9e30bc57597b33b5d7ec6ac759518b62
intermaus1154 <amarco1154@gmail.com>
2025-05-02 10:20:58 +0100

    add post page

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,4 +1,26 @@
 {{--show a specific post detail page--}}
+@props(['post'])
 <x-layout page_title="{{$post->post_title}}">
+    <div class="post-page">
+        <h2>{{$post->post_title}}</h2>
+        {{--only owner or admin can update or delete post--}}
+        @can('update', $post)
+            <div class="post-controls">
+                <a href="" class="action-link blue">Edit</a>
+                <form action="" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="X" class="action-link">
+                </form>
+            </div>
+        @endcan
+        <p class="post-category">
+            Category: {{$post->category->post_category_name}}
+        </p>
+        <p class="post-author">
+            Created by {{$post->author->name}} at {{\Illuminate\Support\Carbon::parse($post->created_at)->toDateString()}}
+        </p>
+        <p class="post-body">{{$post->post_body}}</p>
+    </div>
 
 </x-layout>
